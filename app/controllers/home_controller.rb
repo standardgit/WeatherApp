aabb2a649f0a242c102c896d3d8b4dd60502e85b
Adejare Toluwalope <standardblaise@gmail.com>
2021-08-04 12:00:30 +0100

    Fetching data from an api called airnowApi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=0&API_KEY=D2DE3099-5FDA-42BC-A509-2D923C7E4FD3'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+
+    if @output.empty?
+      @final_output = "Error"
+    else
+      @final_output = @output[0]['AQI']
+    end
+
   end
 end
